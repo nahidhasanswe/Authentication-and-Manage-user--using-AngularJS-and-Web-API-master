ffa332ec1c4fb2e159ba9abf81ecbf2f1d31b2ae
Nahid Hasan <nahid675@diu.edu.bd>
2017-08-12 12:32:51 +0600

    Add project files.

---
+++ SignUp/HTML/AngularJS/change-password.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="angular.js" />
+var changePasswordApp = angular.module('changePasswordApp', ['LocalStorageModule', 'AuthApp']);
+
+changePasswordApp.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+})
+
+changePasswordApp.controller('changePasswordController', ['$scope', '$window', 'authService', function ($scope, $window, authService) {
+    $scope.init = function () {
+        $scope.isProcessing = false;
+        $scope.SubmitBtnText = "Change Password";
+    }
+
+    $scope.init();
+
+    $scope.Password = {
+        OldPassword: "",
+        NewPassword: "",
+        ConfirmPassword:""
+    }
+
+    $scope.ChangePassword = function () {
+        $scope.isProcessing = true;
+        
+        $scope.SubmitBtnText = "Please wait.....";
+
+        authService.changePassword($scope.Password).then(function (response) {
+            alert("Password Changed Successfully");
+            $scope.init();
+        }, function (error) {
+            alert(error.message);
+            $scope.init();
+        })
+    }
+
+}])
